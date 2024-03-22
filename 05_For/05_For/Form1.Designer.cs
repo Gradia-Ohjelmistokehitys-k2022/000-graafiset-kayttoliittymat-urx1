6a7f283f5d611cdf5468e096fb3c5d7f52db9da4
urx1 <121085632+urx1@users.noreply.github.com>
2024-03-22 18:51:31 +0200

    worked on 05

---
+++ 05_For/05_For/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace _05_For
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tb_int = new TextBox();
+            b_start = new Button();
+            l_display = new Label();
+            SuspendLayout();
+            // 
+            // tb_int
+            // 
+            tb_int.Location = new Point(89, 91);
+            tb_int.Name = "tb_int";
+            tb_int.Size = new Size(125, 27);
+            tb_int.TabIndex = 0;
+            tb_int.TextChanged += tb_int_TextChanged;
+            // 
+            // b_start
+            // 
+            b_start.Location = new Point(259, 91);
+            b_start.Name = "b_start";
+            b_start.Size = new Size(94, 29);
+            b_start.TabIndex = 1;
+            b_start.Text = "Start";
+            b_start.UseVisualStyleBackColor = true;
+            b_start.Click += b_start_Click;
+            // 
+            // l_display
+            // 
+            l_display.AutoSize = true;
+            l_display.Location = new Point(192, 216);
+            l_display.Name = "l_display";
+            l_display.Size = new Size(0, 20);
+            l_display.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(l_display);
+            Controls.Add(b_start);
+            Controls.Add(tb_int);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tb_int;
+        private Button b_start;
+        private Label l_display;
+    }
+}
---
+++ 05_For/05_For/Form1.cs
@@ -0,0 +1,48 @@
+using System.Diagnostics.Metrics;
+
+namespace _05_For
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void b_start_Click(object sender, EventArgs e)
+        {
+
+            var updateTask = new Thread(UpdateLabel);
+            updateTask.Start();
+
+            /*int usrInput = int.Parse(tb_int.Text);
+            for (int i = 1; i <= usrInput; i++)
+            //foreach (int value in Enumerable.Range(1, usrInput))
+            {
+                Thread.Sleep(100);
+
+                string valueString = i.ToString();
+                l_display.Text = i.ToString();
+
+            }
+            */
+        }
+
+        private void UpdateLabel()
+        {
+            int usrInput = int.Parse(tb_int.Text);
+            for (int i = 1; i <= usrInput; i++)
+            //foreach (int value in Enumerable.Range(1, usrInput))
+            {
+                string valueString = i.ToString();
+                l_display.Text = i.ToString();
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void tb_int_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
