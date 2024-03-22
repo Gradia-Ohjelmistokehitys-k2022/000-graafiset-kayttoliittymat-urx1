2a81e8e67d8229cfa492c747710e75608dbfacae
urx1 <121085632+urx1@users.noreply.github.com>
2024-03-22 21:11:59 +0200

    maybe finished 06

---
+++ 06_While/06_While/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace _06_While
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            b_start = new Button();
+            SuspendLayout();
+            // 
+            // b_start
+            // 
+            b_start.Location = new Point(110, 116);
+            b_start.Name = "b_start";
+            b_start.Size = new Size(94, 29);
+            b_start.TabIndex = 0;
+            b_start.Text = "Start";
+            b_start.UseVisualStyleBackColor = true;
+            b_start.Click += b_start_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(b_start);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button b_start;
+    }
+}
