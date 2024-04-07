a853e69dece764cd8b27b14c8ce6e0402e2ec887
urx1 <121085632+urx1@users.noreply.github.com>
2024-04-07 15:12:37 +0300

    supreme victory?
    counter bug fixed(?)

--- 08_ArrayLotto/ArrayLotto/Form1.cs
+++ 08_ArrayLotto/ArrayLotto/Form1.cs
@@ -13,10 +13,75 @@ namespace ArrayLotto
         private void b_arvoluvut_Click(object sender, EventArgs e)
         {
             int counter = 0;
+
             int lottonumero;
-            int[] numerot = new int[9];
+            int lisänumero;
+            int[] lottonumerot = new int[7];
+            int[] lisänumerot = new int[2];
+            var kaikkinumerot = lottonumerot.Concat(lisänumerot);
+
             Random rnd = new Random();
 
+            while (counter < 7)
+            {
+                lottonumerot:
+                lottonumero = rnd.Next(2,41);
+
+                foreach (int x in lottonumerot)
+                    if (x == lottonumero)
+                    {
+                        Console.WriteLine("TUPLAT: " + x + ", aloitetaan alusta.");
+                        counter--;
+                        goto lottonumerot;
+                    }
+
+                lottonumerot[counter] = lottonumero;
+
+                Console.WriteLine(counter + " " + lottonumero);
+
+                counter++;
+
+
+            }
+
+            while (counter < 9)
+            {
+            lisänumerot:
+                lisänumero = rnd.Next(2, 41);
+
+               // var kaikkinumerot = lottonumerot.Concat(lisänumerot);
+
+                foreach (int x in kaikkinumerot)
+                    if (x == lisänumero)
+                    {
+                        Console.WriteLine("TUPLAT: " + x + ", aloitetaan alusta.");
+
+                        if ((counter-1) - 7 < 0 || (counter-1) > 2)
+                        {
+                            goto lisänumerot;
+                        }
+
+                        counter--;
+
+                        goto lisänumerot;
+
+                    }
+
+
+                lisänumerot[counter-7] = lisänumero;
+                Console.WriteLine(counter + " " + lisänumero);
+                counter++;
+
+            }
+
+            Console.WriteLine("Numerot arvottu" + " " + counter);
+
+            Array.Sort(lottonumerot);
+            Array.Sort(lisänumerot);
+
+            l_lottonumerot.Text = "Lottonumerot: " + string.Join(", ", lottonumerot) + " Lisänumerot: " + string.Join(",", lisänumerot);
+
+            /*
             while (counter < 9)           
             {
                 alku:
@@ -44,7 +109,7 @@ namespace ArrayLotto
                 Console.WriteLine(counter.ToString() + " " + lottonumero.ToString());
 
             }
-
+            */
         }
     }
 }
