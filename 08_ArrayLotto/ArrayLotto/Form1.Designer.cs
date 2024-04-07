ab34ac415a6e94168f77a20a1f6d8f223f694f3f
urx1 <121085632+urx1@users.noreply.github.com>
2024-04-07 15:17:33 +0300

    phase3

--- 08_ArrayLotto/ArrayLotto/Form1.Designer.cs
+++ 08_ArrayLotto/ArrayLotto/Form1.Designer.cs
@@ -30,11 +30,18 @@
         {
             b_arvoluvut = new Button();
             l_lottonumerot = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
             SuspendLayout();
             // 
             // b_arvoluvut
             // 
-            b_arvoluvut.Location = new Point(345, 155);
+            b_arvoluvut.Location = new Point(356, 161);
             b_arvoluvut.Name = "b_arvoluvut";
             b_arvoluvut.Size = new Size(94, 29);
             b_arvoluvut.TabIndex = 0;
@@ -45,16 +52,73 @@
             // l_lottonumerot
             // 
             l_lottonumerot.AutoSize = true;
-            l_lottonumerot.Location = new Point(386, 88);
+            l_lottonumerot.Location = new Point(402, 96);
             l_lottonumerot.Name = "l_lottonumerot";
             l_lottonumerot.Size = new Size(0, 20);
             l_lottonumerot.TabIndex = 1;
+            l_lottonumerot.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(287, 223);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(30, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(338, 223);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(30, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(386, 223);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(30, 27);
+            textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(479, 223);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(30, 27);
+            textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(237, 223);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(30, 27);
+            textBox5.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(431, 223);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(30, 27);
+            textBox6.TabIndex = 7;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(529, 223);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(30, 27);
+            textBox7.TabIndex = 8;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(l_lottonumerot);
             Controls.Add(b_arvoluvut);
             Name = "Form1";
@@ -67,5 +131,12 @@
 
         private Button b_arvoluvut;
         private Label l_lottonumerot;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
     }
 }
--- 08_ArrayLotto/ArrayLotto/Form1.cs
+++ 08_ArrayLotto/ArrayLotto/Form1.cs
@@ -30,7 +30,7 @@ namespace ArrayLotto
                 foreach (int x in lottonumerot)
                     if (x == lottonumero)
                     {
-                        Console.WriteLine("TUPLAT: " + x + ", aloitetaan alusta.");
+                        Console.WriteLine("TUPLAT: " + x + ", arvotaan uudelleen.");
                         counter--;
                         goto lottonumerot;
                     }
@@ -49,12 +49,10 @@ namespace ArrayLotto
             lisänumerot:
                 lisänumero = rnd.Next(2, 41);
 
-               // var kaikkinumerot = lottonumerot.Concat(lisänumerot);
-
                 foreach (int x in kaikkinumerot)
                     if (x == lisänumero)
                     {
-                        Console.WriteLine("TUPLAT: " + x + ", aloitetaan alusta.");
+                        Console.WriteLine("TUPLAT: " + x + ", arvotaan uudelleen.");
 
                         if ((counter-1) - 7 < 0 || (counter-1) > 2)
                         {
@@ -81,35 +79,6 @@ namespace ArrayLotto
 
             l_lottonumerot.Text = "Lottonumerot: " + string.Join(", ", lottonumerot) + " Lisänumerot: " + string.Join(",", lisänumerot);
 
-            /*
-            while (counter < 9)           
-            {
-                alku:
-                lottonumero = rnd.Next(2, 41);
-
-                foreach (int x in numerot)
-                    if (x == lottonumero)
-                    {
-                        Console.WriteLine("TUPLAT: " + x + ", aloitetaan alusta.");
-                        counter-- ;
-                        goto alku;
-                    }
-
-                numerot[counter] = lottonumero;
-
-                var lottonumerot = numerot[0..7];
-                var lisänumerot = numerot[7..9];
-
-                Array.Sort(lottonumerot);
-                Array.Sort(lisänumerot);
-
-                l_lottonumerot.Text = "Lottonumerot: " + string.Join(", ", lottonumerot) + " Lisänumerot: " + string.Join(",", lisänumerot);
-                counter++;
-
-                Console.WriteLine(counter.ToString() + " " + lottonumero.ToString());
-
-            }
-            */
         }
     }
 }
