87df167879e5d1c28d2b8b1e1e4236dfec6b6b74
urx1 <121085632+urx1@users.noreply.github.com>
2024-04-10 14:27:08 +0300

    continued 14

--- 14_File/File/Form1.cs
+++ 14_File/File/Form1.cs
@@ -1,7 +1,13 @@
+using System.IO;
+using System.Linq.Expressions;
+
 namespace File
 {
     public partial class Form1 : Form
     {
+
+        private String EditorFileName = "Untitled";
+
         public Form1()
         {
             InitializeComponent();
@@ -15,7 +21,9 @@ namespace File
 
         private void newToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            EditorFileName = "Untitled";
+            richTextBox1.Clear();
+            SetFormTitleText();
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
@@ -32,5 +40,43 @@ namespace File
         {
 
         }
+
+        private void SetFormTitleText()
+        {
+            FileInfo fileinfo = new FileInfo(EditorFileName);
+            Text = fileinfo.Name + " - Editor";
+        }
+
+        private void ReadFile()
+        {
+            try
+            {
+                using (StreamReader Reader = new StreamReader(EditorFileName))
+                {
+                    richTextBox1.Clear();
+                    richTextBox1.Text = Reader.ReadToEnd();
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Open File", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void SaveFile()
+        {
+            try
+            {
+                using (StreamWriter StrWriter = new StreamWriter(EditorFileName))
+                {
+                    StrWriter.Write(richTextBox1.Text);
+                }
+            }
+            catch (IOException ex) 
+            {
+                MessageBox.Show("Error: " + ex.Message, "Save File", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }   
+
     }
 }
--- 14_File/File/Program.cs
+++ 14_File/File/Program.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 namespace File
 {
     internal static class Program
