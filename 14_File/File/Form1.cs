0b78ff8e66d96d91d98f26c9378d45708127329f
urx1 <121085632+urx1@users.noreply.github.com>
2024-04-10 13:51:17 +0300

    started 14

---
+++ 14_File/File/Form1.cs
@@ -0,0 +1,15 @@
+namespace File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
