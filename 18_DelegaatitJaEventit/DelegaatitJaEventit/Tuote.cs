b44fd9cbc91f03822de17f8a91b142ac393c9547
urx1 <121085632+urx1@users.noreply.github.com>
2024-04-15 16:15:13 +0300

    created tuote.cs

---
+++ 18_DelegaatitJaEventit/DelegaatitJaEventit/Tuote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegaatitJaEventit
+{
+    public class Tuote
+    {
+        string _nimi;
+        int _paino;
+        int _lukumaara;
+        DateTime _hakupaiva;
+
+
+        public Tuote(string nimi, int paino, int lukumaara, DateTime hakupaiva) 
+        {
+            _nimi = nimi;
+            _paino = paino;
+            _lukumaara = lukumaara;
+            _hakupaiva = hakupaiva;
+        }
+    }
+}
