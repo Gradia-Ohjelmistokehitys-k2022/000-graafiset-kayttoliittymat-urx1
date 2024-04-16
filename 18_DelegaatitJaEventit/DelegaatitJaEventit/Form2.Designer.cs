bd6792abe63e537344f8c3ee36ad71de8aa11ad2
urx1 <121085632+urx1@users.noreply.github.com>
2024-04-16 13:55:07 +0300

    form 2 skeleton

--- 18_DelegaatitJaEventit/DelegaatitJaEventit/Form2.Designer.cs
+++ 18_DelegaatitJaEventit/DelegaatitJaEventit/Form2.Designer.cs
@@ -28,18 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            dateTimePicker1 = new DateTimePicker();
+            l_pvm = new Label();
+            b_tarkista = new Button();
             SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(149, 46);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(250, 27);
+            dateTimePicker1.TabIndex = 0;
+            // 
+            // l_pvm
+            // 
+            l_pvm.AutoSize = true;
+            l_pvm.Location = new Point(39, 51);
+            l_pvm.Name = "l_pvm";
+            l_pvm.Size = new Size(88, 20);
+            l_pvm.TabIndex = 1;
+            l_pvm.Text = "Päivämäärä:";
+            // 
+            // b_tarkista
+            // 
+            b_tarkista.Location = new Point(149, 111);
+            b_tarkista.Name = "b_tarkista";
+            b_tarkista.Size = new Size(169, 29);
+            b_tarkista.TabIndex = 2;
+            b_tarkista.Text = "Tarkista päivämäärä";
+            b_tarkista.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(428, 261);
+            ClientSize = new Size(428, 202);
+            Controls.Add(b_tarkista);
+            Controls.Add(l_pvm);
+            Controls.Add(dateTimePicker1);
             Name = "Form2";
             Text = "Form2";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private Label l_pvm;
+        private Button b_tarkista;
     }
 }
